9cb903306390528cb7dadf84c926e78a4f5f031e
Aman Motghare <aman.motghare69@gmail.com>
2023-03-22 10:37:17 +0530

    second

---
+++ src/ThrowsAdvice/Test.java
@@ -0,0 +1,26 @@
+package ThrowsAdvice;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Resource resource = new ClassPathResource("applicationContext.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		Validator v  = factory.getBean("proxy3",Validator.class);
+		
+		try {
+			v.validate(14);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/ThrowsAdvice/Validator.java
@@ -0,0 +1,14 @@
+package ThrowsAdvice;
+
+public class Validator {
+	
+	public void validate(int age) throws Exception{
+		if(age<18){
+			throw new ArithmeticException("Not Valid Age.");
+		}
+		else{
+			System.out.println("Vote Confirmed.");
+		}
+	}
+	
+}
